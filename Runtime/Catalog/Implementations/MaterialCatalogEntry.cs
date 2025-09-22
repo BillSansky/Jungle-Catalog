b1d42e6fdd138af26e41a7254b5dbfe377dfd11c
Shru <besnard.william@gmail.com>
2025-09-22 21:01:23 +0200

    Add catalog entries for component-related Unity types

---
+++ Runtime/Catalog/Implementations/MaterialCatalogEntry.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Jungle.Catalog.Implementations;
+using UnityEngine;
+
+namespace Plugins.Jungle_Catalog.Runtime.Catalog.Implementations
+{
+    [Serializable]
+    public class MaterialCatalogEntry : GenericCatalogEntry<Material, List<Material>>
+    {
+    }
+}
---
+++ Runtime/Catalog/Implementations/TextureCatalogEntry.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Jungle.Catalog.Implementations;
+using UnityEngine;
+
+namespace Plugins.Jungle_Catalog.Runtime.Catalog.Implementations
+{
+    [Serializable]
+    public class TextureCatalogEntry : GenericCatalogEntry<Texture, List<Texture>>
+    {
+    }
+}
